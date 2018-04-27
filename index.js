5bad99128db7c8a8c98c8421631376106570d68c
Bryan Robles <Bryanaka0@gmail.com>
2018-04-27 14:17:51 -0700

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-deprecation-canary'
+  name: 'ember-cli-deprecation-canary',
+
+  included: function() {
+    // the presence of `this.app.tests` shows that we are in one of:
+    //
+    // * running non-production build
+    // * running tests against production
+    if (this.app.tests) {
+      this.app.import('vendor/ember-cli-deprecation-canary/main.js');
+      this.app.import('vendor/ember-cli-deprecation-canary/deprecation-tracker.js');
+
+      this.app.import('vendor/ember-cli-deprecation-canary/test-adapters/qunit.js');
+      this.app.import('vendor/ember-cli-deprecation-canary/test-adapters.js');
+
+      this.app.import('vendor/ember-cli-deprecation-canary/register-canary.js');
+    }
+  }
 };
