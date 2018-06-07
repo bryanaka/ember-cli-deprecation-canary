aa748a2be5b98e88a96bc9b9ec6015ec8251bcc3
Bryan Robles <Bryanaka0@gmail.com>
2018-06-07 15:51:00 -0700

    dummy test files

---
+++ tests/acceptance/deprecations-test.js
@@ -0,0 +1,4 @@
+// TODO: tests with deprecations that are already recorded
+// TODO: tests with deprecations that are new
+// TODO: tests with deprecations that are new and already recorded
+// all the above with id, text, and regex
